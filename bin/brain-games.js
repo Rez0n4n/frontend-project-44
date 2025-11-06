7806563b59bae0b1dc7a0ca5ca25608227e75e12
Rez0n4n <rez0n4ns@gmail.com>
2025-11-07 00:09:43 +0400

    fix 2

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 const startAnswer = require("../src/cli.js");
-const brainEven = require("../bin/brain-even.js");
 
-const userName = startAnswer();
-brainEven(userName);
+startAnswer();
